ebcc88188caa479ab9084be36059e911147c0f47
root <pravin ranjan>
2020-03-11 12:36:24 +0530

    soda-controller optimization

--- pkg/utils/constants/constants.go
+++ pkg/utils/constants/constants.go
@@ -42,7 +42,7 @@ const (
 
 	// OpensdsConfigPath indicates the absolute path of opensds global
 	// configuration file.
-	OpensdsConfigPath = "/etc/opensds/soda-controller.conf"
+	OpensdsConfigPath = "/etc/opensds/soda-apiserver.conf"
 
 	// OpensdsCtrBindEndpoint indicates the bind endpoint which the opensds
 	// controller grpc server would listen to.
